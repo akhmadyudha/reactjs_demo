c1b0ea857571025e0f147f39d240c19f813b0685
Akhmad <yudha_akhmad@yahoo.com>
2022-11-29 16:13:25 +0700

    header component built

---
+++ src/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Title.css';
+
+const Title = () => {
+  return (
+    <div className="Title">
+      <h1>SFPOPOS</h1>
+        <div className="Title-Subtitle">San Francisco</div>
+    </div>
+  )
+}
+
+export default Title;
