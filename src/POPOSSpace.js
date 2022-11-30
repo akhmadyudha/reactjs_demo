222cb59fbccc1c1bf52c01fa59d1e2bbcf548aad
Akhmad <yudha_akhmad@yahoo.com>
2022-11-30 14:21:52 +0700

    dynamic content and footer

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import logo from './logo.svg'
+import logo from './logo.svg';
+import './POPOSSpace.css';
 
 const POPOSSpace = ({name, image, address}) => {
   return (
-    <div>
+    <div className="POPOSSpace">
       <img src={logo} width="300px" height="300" alt="Hello" />
       <h1>{name}</h1>
       <div>{address}</div>
